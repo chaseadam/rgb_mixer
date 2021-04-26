51a4c3dd9add7240e6d050f07ff7836313c9fa41
Adam Chasen <adam@chasen.name>
2021-04-26 16:26:35 -0400

    broken pass at debounce

--- src/debounce.v
+++ src/debounce.v
@@ -7,4 +7,16 @@ module debounce (
     output reg debounced
 );
 
+    reg [7:0] counter;
+    always @(posedge clk) begin
+        if(reset) begin
+            counter <= 0;
+        end if(button) begin
+            counter <= counter + 1'b1;
+        end else begin
+            counter <= counter - 1'b1;
+        if counter = 1'b11111111 or counter = 1'b00000000 begin
+            assign debounced = counter;
+        end
+    end
 endmodule
