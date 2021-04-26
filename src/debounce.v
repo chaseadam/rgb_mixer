e39e1c811e199580e137192686fee1104d357637
Adam Chasen <adam@chasen.name>
2021-04-26 17:46:43 -0400

    broken pass at debounce

--- src/debounce.v
+++ src/debounce.v
@@ -11,16 +11,19 @@ module debounce (
     always @(posedge clk) begin
         if(reset) begin
             counter <= 0;
-	    debounced <= 1'b0;
-	end else
+            debounced <= 1'b0;
+        end else
+            //counter <= {counter[6:0],button};
             if(button) begin
                 counter <= {counter[6:0],1'b1};
             end else begin
                 counter <= {counter[6:0],1'b0};
-	    end
-            if counter == 8'b11111111 begin
-                debounced <= 1'b1;
             end
-	end
+            if(counter == 8'b11111111) begin
+                debounced <= 1'b1;
+            end else if(counter == 8'b00000000) begin
+                debounced <= 1'b0;
+            end
+        end
     end
 endmodule
