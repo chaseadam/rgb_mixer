341ab89faa7815063160a94f613f2584e06f3aed
Adam Chasen <adam@chasen.name>
2021-04-28 10:17:51 -0400

    parameterize width

--- src/debounce.v
+++ src/debounce.v
@@ -1,26 +1,34 @@
 `default_nettype none
 `timescale 1ns/1ns
-module debounce (
+module debounce #( parameter COUNTER_WIDTH = 8 )(
     input wire clk,
     input wire reset,
     input wire button,
     output reg debounced
 );
+    // make equivalent of binary value full of "ones" (i.e. 8'b11111111)
+    localparam on_value = 2 ** COUNTER_WIDTH - 1;
+    reg [COUNTER_WIDTH-1:0] counter;
 
-    reg [7:0] counter;
     always @(posedge clk) begin
         if(reset) begin
-            counter <= 0;
+            counter <= {COUNTER_WIDTH{1'b0}};
             debounced <= 1'b0;
         end else begin
+            // shortcut to adding to shift register
             //counter <= {counter[6:0],button};
+            // Long form add to shift register, {} is a concatenate
+            // Results in synthesis passing around 
+            // COUNTER_WIDTH - 1 bitwidth...
             if(button)
-                counter <= {counter[6:0],1'b1};
+                counter <= {counter[COUNTER_WIDTH-2:0],1'b1};
             else
-                counter <= {counter[6:0],1'b0};
-            if(counter == 8'b11111111)
+                counter <= {counter[COUNTER_WIDTH-2:0],1'b0};
+            if(counter == on_value)
                 debounced <= 1'b1;
-            else if(counter == 8'b00000000)
+            // This gets converted to a "not" logic check in synthesis
+            // nested {{}} is "replication" operator
+            else if(counter == {COUNTER_WIDTH{1'b0}})
                 debounced <= 1'b0;
         end
     end
