1e6805100b8a189032e31b5b936942eb70478abe
Adam Chasen <adam@chasen.name>
2021-04-28 13:57:31 -0400

    rgb test pass

--- src/rgb_mixer.v
+++ src/rgb_mixer.v
@@ -19,36 +19,36 @@ module rgb_mixer (
     wire [7:0] enc0, enc1, enc2;
 
 
-    reg [0:0] clk_slow;
-    
+    reg clk_slow;
     reg [7:0] counter;
 
     always @(posedge clk) begin
         if(reset) begin
             counter <= 0;
+            clk_slow <= 0;
         end else begin
             counter <= counter + 1'b1;
-        if(counter[7:7] == 1'b1)
-            clk_slow <= clk_slow + 1'b1;
+            //if(counter[7:7] == 1'b1)
+            clk_slow <= ~clk_slow;
         end
     end
 
-    debounce #(.COUNTER_WIDTH(8)) debounce0_a(.clk(clk_slow), .reset(reset), .button(enc0_a), .debounced(enc0_a_db));
-    debounce #(.COUNTER_WIDTH(8)) debounce0_b(.clk(clk_slow), .reset(reset), .button(enc0_b), .debounced(enc0_b_db));
+    debounce #(.COUNTER_WIDTH(8)) debounce0_a(.clk(clk), .reset(reset), .button(enc0_a), .debounced(enc0_a_db));
+    debounce #(.COUNTER_WIDTH(8)) debounce0_b(.clk(clk), .reset(reset), .button(enc0_b), .debounced(enc0_b_db));
 
-    debounce #(.COUNTER_WIDTH(8)) debounce1_a(.clk(clk_slow), .reset(reset), .button(enc1_a), .debounced(enc1_a_db));
-    debounce #(.COUNTER_WIDTH(8)) debounce1_b(.clk(clk_slow), .reset(reset), .button(enc1_b), .debounced(enc1_b_db));
+    debounce #(.COUNTER_WIDTH(8)) debounce1_a(.clk(clk), .reset(reset), .button(enc1_a), .debounced(enc1_a_db));
+    debounce #(.COUNTER_WIDTH(8)) debounce1_b(.clk(clk), .reset(reset), .button(enc1_b), .debounced(enc1_b_db));
 
-    debounce #(.COUNTER_WIDTH(8)) debounce2_a(.clk(clk_slow), .reset(reset), .button(enc2_a), .debounced(enc2_a_db));
-    debounce #(.COUNTER_WIDTH(8)) debounce2_b(.clk(clk_slow), .reset(reset), .button(enc2_b), .debounced(enc2_b_db));
+    debounce #(.COUNTER_WIDTH(8)) debounce2_a(.clk(clk), .reset(reset), .button(enc2_a), .debounced(enc2_a_db));
+    debounce #(.COUNTER_WIDTH(8)) debounce2_b(.clk(clk), .reset(reset), .button(enc2_b), .debounced(enc2_b_db));
 
     //encoders
-    encoder encoder0(.clk(clk_slow), .reset(reset), .a(enc0_a_db), .b(enc0_b_db), .value(enc0));
-    encoder encoder1(.clk(clk_slow), .reset(reset), .a(enc1_a_db), .b(enc1_b_db), .value(enc1));
-    encoder encoder2(.clk(clk_slow), .reset(reset), .a(enc2_a_db), .b(enc2_b_db), .value(enc2));
+    encoder encoder0(.clk(clk), .reset(reset), .a(enc0_a_db), .b(enc0_b_db), .value(enc0));
+    encoder encoder1(.clk(clk), .reset(reset), .a(enc1_a_db), .b(enc1_b_db), .value(enc1));
+    encoder encoder2(.clk(clk), .reset(reset), .a(enc2_a_db), .b(enc2_b_db), .value(enc2));
 
     //pwm
-    pwm pwm0(.clk(clk_slow), .reset(reset), .out(pwm0_out), .level(enc0));
-    pwm pwm1(.clk(clk_slow), .reset(reset), .out(pwm1_out), .level(enc1));
-    pwm pwm2(.clk(clk_slow), .reset(reset), .out(pwm2_out), .level(enc2));
+    pwm pwm0(.clk(clk), .reset(reset), .out(pwm0_out), .level(enc0));
+    pwm pwm1(.clk(clk), .reset(reset), .out(pwm1_out), .level(enc1));
+    pwm pwm2(.clk(clk), .reset(reset), .out(pwm2_out), .level(enc2));
 endmodule
